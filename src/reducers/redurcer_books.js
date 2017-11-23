2c3a41855a525e1a46ef848e989a0eeeb69b43a6
Djordje Stojiljkovic <djordje.stojiljkovic@telenor.rs>
2017-11-23 14:20:20 +0100

    Initial React learn redux

---
+++ src/reducers/redurcer_books.js
@@ -0,0 +1,20 @@
+export default () => {
+    return [
+        {
+            title: 'Javascript: The Good Parts',
+            pages: 100
+        },
+        {
+            title: 'Harry Potter',
+            pages: 200
+        },
+        {
+            title: 'The Dark Tower',
+            pages: 560
+        },
+        {
+            title: 'Eloquent Ruby',
+            pages: 10
+        }
+    ]
+}
